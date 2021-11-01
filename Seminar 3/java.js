321b8e1e76a6b0c729f9cd57570947ee8c3c285f
iri02000 <arinjeong1998@gmail.com>
2021-11-01 19:10:39 +0200

    commit

---
+++ Seminar 3/java.js
@@ -0,0 +1,34 @@
+function ShowAlert(){
+    alert (" Warning! You are about to reset your inputs. Are you sure you want tocontinue?”!")
+}
+
+function Congrats(){
+    alert(" Congrats! You have just made your account!")
+}
+// const form= document.getElementById ('form');
+// const name= document.getElementById ('name');
+// const email= document.getElementById ('email');
+// const password1= document.getElementById ('password1');
+// const password2= document.getElementById ('password2');
+
+// const namevalue = name.value.trim();
+// const emailvalue = email.value.trim();
+// const password1value = email.value.password1();
+// const password2value = email.value.password2();
+
+// if ( namevalue==='' || email==='' || password1value ==='' || password2value==='')
+// EmptyAlert();
+
+let sum=0;
+for(let i=1 ;i<51;i++)
+{
+    sum+=i;
+ 
+}
+ console.log ("Sum of all numbers up to 50: " +sum)
+
+
+
+
+
+
