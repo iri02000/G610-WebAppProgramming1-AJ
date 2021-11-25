7b9cee15160c2bb71b716fd7f96e1ff2444f97df
iri02000 <arinjeong1998@gmail.com>
2021-11-25 10:52:57 +0200

    comm

--- Seminar 5/Exercitii_Seminar5.py
+++ Seminar 5/Exercitii_Seminar5.py
@@ -7,39 +7,39 @@
 # print(3-2)
 # print(3*2)
 
-print("Finding the percentage"
-      )  #incomplet pentru ca nu l-am facut in dictionary data type
+# print("Finding the percentage"
+#       )  #incomplet pentru ca nu l-am facut in dictionary data type
 
-n = int(input("Cati elevi sunt? "))
+# n = int(input("Cati elevi sunt? "))
 
-list = []
-i = 0
-while i < n:
-    list.append(input("Input the elements : ").split())
-    print(list[i])
-    i = i + 1
-do = True
-while do == True:
-    name = input("Media cui vreti sa o calculam? ")
-    i = 0
-    e = False
+# list = []
+# i = 0
+# while i < n:
+#     list.append(input("Input the elements : ").split())
+#     print(list[i])
+#     i = i + 1
+# do = True
+# while do == True:
+#     name = input("Media cui vreti sa o calculam? ")
+#     i = 0
+#     e = False
 
-    while i < n:
-        if list[i][0] == name:
-            average = float(
-                (int(list[i][1]) + int(list[i][2]) + int(list[i][3])) /
-                (len(list[i]) - 1))
-            average_2decimal = "{:.2f}".format(average)
-            print("Media lui ", name, "este ", average_2decimal)
-            e = True
-        i = i + 1
-    if e == False:
-        print("Numele nu exista! ")
-    rasp = input("Doriti sa mai aflati media cuiva? Raspundeti cu da sau nu. ")
-    if rasp == "da":
-        do = True
-    else:
-        do = False
+#     while i < n:
+#         if list[i][0] == name:
+#             average = float(
+#                 (int(list[i][1]) + int(list[i][2]) + int(list[i][3])) /
+#                 (len(list[i]) - 1))
+#             average_2decimal = "{:.2f}".format(average)
+#             print("Media lui ", name, "este ", average_2decimal)
+#             e = True
+#         i = i + 1
+#     if e == False:
+#         print("Numele nu exista! ")
+#     rasp = input("Doriti sa mai aflati media cuiva? Raspundeti cu da sau nu. ")
+#     if rasp == "da":
+#         do = True
+#     else:
+#         do = False
 
 # print("Python if else")
 
@@ -154,3 +154,70 @@ while do == True:
 # t = (tuple(t_el))
 # print(t)
 # print(hash(t))
+
+# def average(marks):
+#     avg = 0
+#     for mark in marks:
+#         avg = avg + mark
+#     avg = avg / len(marks)
+#     return round(avg, 2)
+
+# n = input("Please enter the number of students: ")
+# n = int(n)
+
+# lines = []
+# for i in range(n):
+#     line = input()
+#     lines.append(line)
+
+# # catalogue = {
+# #     "andrei": [10, 20, 30]
+# # }
+# catalogue = {}
+# for line in lines:
+#     line_elements = line.split(" ")
+#     student_name = line_elements[0]
+#     n_marks = len(line_elements[1:])
+#     student_marks = []
+#     for i in range(1, n_marks + 1):
+#         student_mark = float(line_elements[i])
+#         student_marks.append(student_mark)
+#     catalogue[student_name] = student_marks
+
+# current_student = input("Please enter student's name: ")
+# students = list(catalogue.keys())
+
+# if current_student in students:
+#     avg = average(catalogue[current_student])
+#     print(f"Student {current_student} has an average of {avg}.")
+# else:
+#     print(
+#         f"Student {current_student} not found. Please try again using one of the following students: {students}."
+#     )
+
+# n = int(input("Cate numere?"))
+# line = input()
+# line_elements = []
+# for element in line:
+#     line_elements = line.split(" ")
+# line_elements = [int(i) for i in line_elements]
+
+# line_elements.sort()
+# print(line_elements)
+
+# count = 0
+# pos = len(line_elements)
+# while line_elements[len(line_elements)] == line_elements[pos]:
+#     count = count + 1
+#     pos = pos - 1
+
+# i = 0
+# while i < count:
+#     line_elements.pop()
+#     i = i + 1
+
+# print(line_elements[len(line_elements)])
+
+# from Flask
+
+# app= Flask ("my app")
